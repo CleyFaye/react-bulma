649ff27981cc44e2250a4c3ce24f6d9a1ef0eccd
Gabriel Paul 'Cley Faye' Risterucci <cleyfaye@gmail.com>
2021-06-05 20:14:56 +0200

    Add license to output

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,14 @@
 const loadGruntTasks = require("load-grunt-tasks");
+const {readFileSync} = require("fs");
+
+const licenseJS = [
+  "/**",
+  " * @license",
+  " * @preserve",
+  ...readFileSync("LICENSE", "utf8").split("\n")
+    .map(c => ` * ${c}`.trimEnd()),
+  " */",
+].join("\n");
 
 module.exports = grunt => {
   loadGruntTasks(grunt);
@@ -36,12 +46,23 @@ module.exports = grunt => {
         }],
       },
     },
+    usebanner: {
+      options: {banner: licenseJS},
+      build: {
+        files: [{
+          expand: true,
+          cwd: "lib",
+          src: ["**/*.js"],
+        }],
+      },
+    },
   });
   grunt.registerTask(
     "build",
     "Build library",
     [
       "babel:build",
+      "usebanner:build",
     ]
   );
   grunt.registerTask(
